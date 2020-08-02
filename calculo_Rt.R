fdd12768bbae03b7169d93328abb9a8866d44cab
Leandro Pereira Garcia <lpgarcia18@gmail.com>
2020-08-02 14:14:53 -0300

    Rt 14 dias

--- calculo_Rt.R
+++ calculo_Rt.R
@@ -35,7 +35,6 @@ for(i in seq_along(covid$INICIO_SINTOMAS)){
 	
 	Icovid <- incidencia$MEDIANA_CASOS
 	gencovid <- EpiEstim::discr_si(c(0:max(incidencia$MEDIANA_CASOS)), mu = 4.8, sigma = 2.3)
-	sum(gencovid, na.rm = T) #Analisando para ver se a soma da probabilidade é 1
 	Lcovid = overall_infectivity(Icovid, gencovid)
 	
 	
@@ -63,8 +62,10 @@ decretos <- read_csv("dados/classificacao_decretos.csv")
 anterior_publicacao <- (as.Date(decretos$PUBLICACAO, format = "%d/%m/%Y") - 1) %>% as.character() #Selecionando Rt anterior a data da publicação do decreto
 res_base_diarios_sel <- subset(res_base_diarios, as.character(names(res_base_diarios)) %in% as.character(anterior_publicacao)) 
 res_base_diarios_sel <- res_base_diarios_sel %>% 
-	lapply(function(x)tail(x,5)) %>%
-	lapply(function(x)mutate(x,DEFASAGEM = c("D_5","D_4","D_3","D_2","D_1"))) %>%
+	lapply(function(x)tail(x,14)) %>%
+	lapply(function(x)mutate(x,DEFASAGEM = c("D_14","D_13","D_12","D_11",
+						 "D_10","D_9","D_8","D_7","D_6",
+						 "D_5","D_4","D_3","D_2","D_1"))) %>%
 	lapply(function(x)mutate(x,DATA = NULL)) %>%
 	lapply(function(x)melt(x, id.var = "DEFASAGEM"))  %>%
 	lapply(function(x)dcast(x,formula = . ~ DEFASAGEM + variable , fun.aggregate = NULL))
@@ -82,13 +83,31 @@ for(i in seq_along(decretos_analies$PUBLICACAO)){
 	       decretos_analies$D_2_Rt_975[i] >= 1 |
 	       decretos_analies$D_3_Rt_975[i] >= 1 |
 	       decretos_analies$D_4_Rt_975[i] >= 1 |
-	       decretos_analies$D_5_Rt_975[i] >= 1) & (
+	       decretos_analies$D_5_Rt_975[i] >= 1 |
+	       decretos_analies$D_6_Rt_975[i] >= 1 |
+	       decretos_analies$D_7_Rt_975[i] >= 1 |
+	       decretos_analies$D_8_Rt_975[i] >= 1 |
+	       decretos_analies$D_9_Rt_975[i] >= 1 |
+	       decretos_analies$D_10_Rt_975[i] >= 1 |
+	       decretos_analies$D_11_Rt_975[i] >= 1 |
+	       decretos_analies$D_12_Rt_975[i] >= 1 |
+	       decretos_analies$D_13_Rt_975[i] >= 1 |
+	       decretos_analies$D_14_Rt_975[i] >= 1 ) & (
 	       decretos_analies$CLASSIFICACAO[i] == "Aumenta medidas de distanciamento")) |
 	     ((decretos_analies$D_1_Rt_975[i] < 1 & #R<1 todos os dias e Flexibiliza medidas de distanciamento = Consonante
 	       decretos_analies$D_2_Rt_975[i] < 1 &
 	       decretos_analies$D_3_Rt_975[i] < 1 &
 	       decretos_analies$D_4_Rt_975[i] < 1 &
-	       decretos_analies$D_5_Rt_975[i] < 1) & (
+	       decretos_analies$D_5_Rt_975[i] < 1 &
+	       decretos_analies$D_6_Rt_975[i] < 1 &
+	       decretos_analies$D_7_Rt_975[i] < 1 &
+	       decretos_analies$D_8_Rt_975[i] < 1 &
+	       decretos_analies$D_9_Rt_975[i] < 1 &
+	       decretos_analies$D_10_Rt_975[i] < 1 &
+	       decretos_analies$D_11_Rt_975[i] < 1 &
+	       decretos_analies$D_12_Rt_975[i] < 1 &
+	       decretos_analies$D_13_Rt_975[i] < 1 &
+	       decretos_analies$D_14_Rt_975[i] < 1) & (
 	       decretos_analies$CLASSIFICACAO[i] == "Flexibiliza medidas de distanciamento"))
 	     ){decretos_analies$ANALISE[i] <- "Consonante"
 	      }else{
@@ -100,20 +119,58 @@ decr <- decretos_analies
 decr$D_1_Rt <- round(decr$D_1_Rt,3)
 decr$D_1_Rt_025 <- round(decr$D_1_Rt_025,3)
 decr$D_1_Rt_975 <- round(decr$D_1_Rt_975,3)
+
 decr$D_2_Rt <- round(decr$D_2_Rt,3)
 decr$D_2_Rt_025 <- round(decr$D_2_Rt_025,3)
 decr$D_2_Rt_975 <- round(decr$D_2_Rt_975,3)
+
 decr$D_3_Rt <- round(decr$D_3_Rt,3)
 decr$D_3_Rt_025 <- round(decr$D_3_Rt_025,3)
 decr$D_3_Rt_975 <- round(decr$D_3_Rt_975,3)
+
 decr$D_4_Rt <- round(decr$D_4_Rt,3)
 decr$D_4_Rt_025 <- round(decr$D_4_Rt_025,3)
 decr$D_4_Rt_975 <- round(decr$D_4_Rt_975,3)
+
 decr$D_5_Rt <- round(decr$D_5_Rt,3)
 decr$D_5_Rt_025 <- round(decr$D_5_Rt_025,3)
 decr$D_5_Rt_975 <- round(decr$D_5_Rt_975,3)
 
+decr$D_6_Rt <- round(decr$D_6_Rt,3)
+decr$D_6_Rt_025 <- round(decr$D_6_Rt_025,3)
+decr$D_6_Rt_975 <- round(decr$D_6_Rt_975,3)
 
+decr$D_7_Rt <- round(decr$D_7_Rt,3)
+decr$D_7_Rt_025 <- round(decr$D_7_Rt_025,3)
+decr$D_7_Rt_975 <- round(decr$D_7_Rt_975,3)
+
+decr$D_8_Rt <- round(decr$D_8_Rt,3)
+decr$D_8_Rt_025 <- round(decr$D_8_Rt_025,3)
+decr$D_8_Rt_975 <- round(decr$D_8_Rt_975,3)
+
+decr$D_9_Rt <- round(decr$D_9_Rt,3)
+decr$D_9_Rt_025 <- round(decr$D_9_Rt_025,3)
+decr$D_9_Rt_975 <- round(decr$D_9_Rt_975,3)
+
+decr$D_10_Rt <- round(decr$D_10_Rt,3)
+decr$D_10_Rt_025 <- round(decr$D_10_Rt_025,3)
+decr$D_10_Rt_975 <- round(decr$D_10_Rt_975,3)
+
+decr$D_11_Rt <- round(decr$D_11_Rt,3)
+decr$D_11_Rt_025 <- round(decr$D_11_Rt_025,3)
+decr$D_11_Rt_975 <- round(decr$D_11_Rt_975,3)
+
+decr$D_12_Rt <- round(decr$D_12_Rt,3)
+decr$D_12_Rt_025 <- round(decr$D_12_Rt_025,3)
+decr$D_12_Rt_975 <- round(decr$D_12_Rt_975,3)
+
+decr$D_13_Rt <- round(decr$D_13_Rt,3)
+decr$D_13_Rt_025 <- round(decr$D_13_Rt_025,3)
+decr$D_13_Rt_975 <- round(decr$D_13_Rt_975,3)
+
+decr$D_14_Rt <- round(decr$D_14_Rt,3)
+decr$D_14_Rt_025 <- round(decr$D_14_Rt_025,3)
+decr$D_14_Rt_975 <- round(decr$D_14_Rt_975,3)
 
 
 decr$D1 <- paste0(decr$D_1_Rt," (", decr$D_1_Rt_025, "-", decr$D_1_Rt_975, ")")
@@ -121,9 +178,20 @@ decr$D2 <- paste0(decr$D_2_Rt," (", decr$D_2_Rt_025, "-", decr$D_2_Rt_975, ")")
 decr$D3 <- paste0(decr$D_3_Rt," (", decr$D_3_Rt_025, "-", decr$D_3_Rt_975, ")")
 decr$D4 <- paste0(decr$D_4_Rt," (", decr$D_4_Rt_025, "-", decr$D_4_Rt_975, ")")
 decr$D5 <- paste0(decr$D_5_Rt," (", decr$D_5_Rt_025, "-", decr$D_5_Rt_975, ")")
+decr$D6 <- paste0(decr$D_6_Rt," (", decr$D_6_Rt_025, "-", decr$D_6_Rt_975, ")")
+decr$D7 <- paste0(decr$D_7_Rt," (", decr$D_7_Rt_025, "-", decr$D_7_Rt_975, ")")
+decr$D8 <- paste0(decr$D_8_Rt," (", decr$D_8_Rt_025, "-", decr$D_8_Rt_975, ")")
+decr$D9 <- paste0(decr$D_9_Rt," (", decr$D_9_Rt_025, "-", decr$D_9_Rt_975, ")")
+decr$D10 <- paste0(decr$D_10_Rt," (", decr$D_10_Rt_025, "-", decr$D_10_Rt_975, ")")
+decr$D11 <- paste0(decr$D_11_Rt," (", decr$D_11_Rt_025, "-", decr$D_11_Rt_975, ")")
+decr$D12 <- paste0(decr$D_12_Rt," (", decr$D_12_Rt_025, "-", decr$D_12_Rt_975, ")")
+decr$D13 <- paste0(decr$D_13_Rt," (", decr$D_13_Rt_025, "-", decr$D_13_Rt_975, ")")
+decr$D14 <- paste0(decr$D_14_Rt," (", decr$D_14_Rt_025, "-", decr$D_14_Rt_975, ")")
+
 
 #Escrevendo o resultado
 write.csv(decr, "resultados/resultado.csv", row.names = F)
+
 
  
 
